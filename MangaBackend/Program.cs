85150b19883d0c7b27c0419239cb4a3c456238ac
YakovAkk <james.yasko.2002@gmail.com>
2022-09-14 15:13:55 +0300

    was added swagger to release version

--- MangaBackend/Program.cs
+++ MangaBackend/Program.cs
@@ -81,11 +81,10 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 
     app.UseCors(options =>
     {
