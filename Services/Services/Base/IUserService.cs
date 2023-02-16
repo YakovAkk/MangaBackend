6cfbe7df993a4ce532ec595063e46a12770e4cbd
Andrii <andrii.buchman@kitrum.com>
2023-02-16 13:59:25 +0200

    complete

--- Services/Services/Base/IUserService.cs
+++ Services/Services/Base/IUserService.cs
@@ -7,11 +7,11 @@ public interface IUserService
 {
     Task<UserEntity> GetUserByNameOrEmail(string nameOrEmail);
     Task<IList<UserEntity>> GetAllAsync();
-    Task<UserEntity> UpdateAsync(UserEntity user);
-    Task<UserEntity> AddMangaToFavoriteAsync(FavoriteDTO addTOFavoriteDTO);
-    Task<UserEntity> AddGenreToFavoriteAsync(FavoriteDTO addTOFavoriteDTO);
-    Task<UserEntity> RemoveGenreFromFavoriteAsync(string userid, string genreid);
-    Task<UserEntity> RemoveMangaFromFavoriteAsync(string userid, string mangaid);
+    Task<bool> UpdateAsync(UserEntity user);
+    Task<bool> AddMangaToFavoriteAsync(FavoriteDTO addTOFavoriteDTO);
+    Task<bool> AddGenreToFavoriteAsync(FavoriteDTO addTOFavoriteDTO);
+    Task<bool> RemoveGenreFromFavoriteAsync(string userid, string genreid);
+    Task<bool> RemoveMangaFromFavoriteAsync(string userid, string mangaid);
     Task<IList<MangaEntity>> GetAllFavoriteMangaAsync(string userid);
     Task<IList<GenreEntity>> GetAllFavoriteGenreAsync(string userid);
     Task<UserEntity> GetByIdAsync(string user_id);
